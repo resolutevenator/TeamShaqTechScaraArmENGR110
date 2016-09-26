2239133bea9bf7c5f2c4bca95a5a70a000ddc3b2
koyawase <waseemkoya@gmail.com>
2016-09-26 13:20:23 +1300

    printing x,y values of pen

--- Dank as software boi/ToolPath.java
+++ Dank as software boi/ToolPath.java
@@ -1,5 +1,3 @@
- 
-
 
 /**
  * ToolPath stores motor contol signals (pwm)
@@ -24,20 +22,19 @@ import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-
 public class ToolPath
 {
-     int n_steps; //straight line segmentt will be broken
-                      // into that many sections
-                      
-     // storage for angles and 
-     // moto control signals
-     ArrayList<Double> theta1_vector;
-     ArrayList<Double> theta2_vector;
-     ArrayList<Integer> pen_vector;
-     ArrayList<Integer> pwm1_vector;
-     ArrayList<Integer> pwm2_vector;
-     ArrayList<Integer> pwm3_vector;
+    int n_steps; //straight line segmentt will be broken
+    // into that many sections
+
+    // storage for angles and 
+    // moto control signals
+    ArrayList<Double> theta1_vector;
+    ArrayList<Double> theta2_vector;
+    ArrayList<Integer> pen_vector;
+    ArrayList<Integer> pwm1_vector;
+    ArrayList<Integer> pwm2_vector;
+    ArrayList<Integer> pwm3_vector;
 
     /**
      * Constructor for objects of class ToolPath
@@ -45,13 +42,13 @@ public class ToolPath
     public ToolPath()
     {
         // initialise instance variables
-      n_steps = 50;
-      theta1_vector = new ArrayList<Double>();
-      theta2_vector = new ArrayList<Double>();
-      pen_vector = new ArrayList<Integer>();
-      pwm1_vector = new ArrayList<Integer>();
-      pwm2_vector = new ArrayList<Integer>();
-      pwm3_vector = new ArrayList<Integer>();
+        n_steps = 50;
+        theta1_vector = new ArrayList<Double>();
+        theta2_vector = new ArrayList<Double>();
+        pen_vector = new ArrayList<Integer>();
+        pwm1_vector = new ArrayList<Integer>();
+        pwm2_vector = new ArrayList<Integer>();
+        pwm3_vector = new ArrayList<Integer>();
 
     }
 
@@ -71,21 +68,21 @@ public class ToolPath
                 theta1_vector.add(arm.get_theta1()*180/Math.PI);
                 theta2_vector.add(arm.get_theta2()*180/Math.PI);
                 if (p0.get_pen()){ 
-                  pen_vector.add(1);
+                    pen_vector.add(1);
                 } else {
-                  pen_vector.add(0);
+                    pen_vector.add(0);
                 }
             }
         }
     }
-    
+
     public void save_angles(String fname){
         for ( int i = 0 ; i < theta1_vector.size(); i++){
-         UI.printf(" t1=%3.1f t2=%3.1f pen=%d\n",
-            theta1_vector.get(i),theta2_vector.get(i),pen_vector.get(i));
+            UI.printf(" t1=%3.1f t2=%3.1f pen=%d\n",
+                theta1_vector.get(i),theta2_vector.get(i),pen_vector.get(i));
         }
-        
-         try {
+
+        try {
             //Whatever the file path is.
             File statText = new File(fname);
             FileOutputStream is = new FileOutputStream(statText);
@@ -94,16 +91,16 @@ public class ToolPath
             String str_out;
             for (int i = 1; i < theta1_vector.size() ; i++){
                 str_out = String.format("%3.1f,%3.1f,%d\n",
-                  theta1_vector.get(i),theta2_vector.get(i),pen_vector.get(i));
+                    theta1_vector.get(i),theta2_vector.get(i),pen_vector.get(i));
                 w.write(str_out);
             }
             w.close();
         } catch (IOException e) {
             UI.println("Problem writing to the file statsTest.txt");
         }
-        
+
     }
-    
+
     // takes sequence of angles and converts it 
     // into sequence of motor signals
     public void convert_angles_to_pwm(Arm arm){
@@ -114,10 +111,39 @@ public class ToolPath
             pwm2_vector.add(arm.get_pwm2());
         }
     }
-    
-    // save file with motor control values
+
+    /** save file with motor control values
     public void save_pwm_file(){
-        ...
+    //...
+    }*/
+
+    //save file with motor control values
+    public void save_pwm_file(String fname){
+
+        for ( int i = 0 ; i < pwm1_vector.size(); i++){
+            UI.printf(" pwm1=%d pwm2=%d pwm3=%d\n",
+                pwm1_vector.get(i), pwm2_vector.get(i), pwm3_vector.get(i));    
+        }
+
+        try {
+
+            File statText = new File(fname);
+            FileOutputStream is = new FileOutputStream(statText);
+            OutputStreamWriter osw = new OutputStreamWriter(is);    
+            Writer w = new BufferedWriter(osw);
+            String str_out;
+
+            for (int i = 0; i < pwm1_vector.size() ; i++){
+                str_out = String.format("%d,%d,%d\n",
+                    pwm1_vector.get(i), pwm2_vector.get(i), pwm3_vector.get(i));
+                w.write(str_out);
+            }
+            w.close();
+        } catch (IOException e) {
+            UI.println("Problem writing to the file statsTest.txt");
+        }
+
     }
 
 }
+
